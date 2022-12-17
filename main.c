250380137545ce51853dd2d1d0235c9fececa7f2
Anthony <anthony_github@crontab.pw>
2022-12-17 11:01:03 -0500

    Added rudimentary functions to initialize and terminate engine

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <unistd.h>
+
+#define PROGRAM_NAME "mayflowerTextEngine"
+
+void initializeEngine();
+void terminateEngine();
 
 int main (void)
 {
+	initializeEngine();
+	terminateEngine();
 	return 0;
 }
+
+void initializeEngine()
+{
+	printf("\e[1;1H\e[2J");	/* clears the screen */
+	printf("initializing %s...\n", PROGRAM_NAME);
+	printf("Copyright (c) 2022 www.crontab.pw\n");
+	sleep(1);
+}
+
+void terminateEngine()
+{
+        printf("\e[1;1H\e[2J"); /* clears the screen */
+}
+
