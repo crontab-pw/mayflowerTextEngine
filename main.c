e461a08f4780a3beae279f4911804ccd54ab25dc
Anthony <anthony_github@crontab.pw>
2022-12-17 22:14:55 -0500

    Added function and pointer to determine size of user's terminal

--- main.c
+++ main.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 
 #define PROGRAM_NAME "mayflowerTextEngine"
 #define COPYRIGHT_DATE "2022"
 #define CREATOR_NAME "www.crontab.pw"
 
+int* determineTerminalSize();
 void initializeEngine();
 void mainLoop();
 void terminateEngine();
 
 int main (void)
 {
+	int *totalRowsColumns = determineTerminalSize();
 	initializeEngine();
 	mainLoop();
 	terminateEngine();
 	return 0;
+}
+
+int* determineTerminalSize()
+{
+	static int totalRowsColumns[2];	/* [0] = rows [1] = columns */
+        struct winsize max;
+        ioctl(0, TIOCGWINSZ , &max);
+	totalRowsColumns[0] = max.ws_row;
+	totalRowsColumns[1] = max.ws_col;
+	return totalRowsColumns;
 }
 
 void initializeEngine()
