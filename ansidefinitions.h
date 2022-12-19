9c9fb8271812931752f7de92fb5af8d5362811a4
Anthony <anthony_github@crontab.pw>
2022-12-19 10:41:24 -0500

    Made all escape sequences uniform

--- ansidefinitions.h
+++ ansidefinitions.h
@@ -1,6 +1,7 @@
 #define CLEAR_SCREEN    "\e[2J"
-#define CURSOR_HOME	"\e[H"
-#define CURSOR_LAST	"\e[%d;0H"
-#define DEFAULT_COLORS  "\x1b[39;49m"
-#define DRAW_TITLEBAR	"\x1b[2;44;44m "
-#define DISPLAY_TITLE	"\e[1;%luH%s"
+#define CURSOR_HOME     "\e[H"
+#define CURSOR_LAST     "\e[%d;0H"
+#define DEFAULT_COLORS  "\e[39;49m"
+#define DRAW_TITLEBAR   "\e[2;44;44m "
+#define DISPLAY_TITLE   "\e[1;%luH%s"
+
