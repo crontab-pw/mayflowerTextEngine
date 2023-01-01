283434d6a5bb3bcd20408850a3bc704846f1103c
Anthony <anthony_github@crontab.pw>
2023-01-01 14:21:55 -0500

    Updated COPYRIGHT_DATE to reflect new year

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 
 #define PROGRAM_NAME "mayflowerTextEngine"
 #define COMMAND_PROMPT "<> "
-#define COPYRIGHT_DATE "2022"
+#define COPYRIGHT_DATE "2022-2023"
 #define CREATOR_NAME "www.crontab.pw"
 #define MAXIMUM_SIZE 200
 #define MAXIMUM_TOKENS 2 /* Represents elements in array. 0 = first element */ 
