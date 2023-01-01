0c52437168fba4eaaa6a9621d69865af03b1a850
Anthony <anthony_github@crontab.pw>
2023-01-01 13:56:24 -0500

    Restuctured main.c to allow commands.h to access definitions

--- commands.h
+++ commands.h
@@ -1,12 +1,3 @@
-#define PROGRAM_NAME "mayflowerTextEngine"
-#define COMMAND_PROMPT "<> "
-#define COPYRIGHT_DATE "2022"
-#define CREATOR_NAME "www.crontab.pw"
-#define MAXIMUM_SIZE 200
-#define MAXIMUM_TOKENS 2 /* Represents elements in array. 0 = first element */
-#define OFF 0
-#define ON 1
-
 int call_function(const char *name);
 void commandAbout();
 void commandHelp();
--- main.c
+++ main.c
@@ -4,9 +4,6 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 
-#include "ansidefinitions.h"
-#include "commands.h"
-
 #define PROGRAM_NAME "mayflowerTextEngine"
 #define COMMAND_PROMPT "<> "
 #define COPYRIGHT_DATE "2022"
@@ -15,6 +12,9 @@
 #define MAXIMUM_TOKENS 2 /* Represents elements in array. 0 = first element */ 
 #define OFF 0
 #define ON 1
+
+#include "ansidefinitions.h"
+#include "commands.h"
 
 int* determineTerminalSize();
 void displayCommandLine();
